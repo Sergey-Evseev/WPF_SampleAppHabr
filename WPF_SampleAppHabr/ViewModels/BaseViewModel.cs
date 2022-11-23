619741f30314994cea51658739de17addfc1634e
Sergey Evseev <sevseev@gmail.com>
2022-11-23 17:33:52 +0300

    Add project files.

---
+++ WPF_SampleAppHabr/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SampleAppHabr.ViewModels
+{ //базовая вьюмодель
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPF_SampleAppHabr/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SampleAppHabr.ViewModels
+{
+    //вьюмодель для баового класса, наследуется от базовой вьюмодели
+    public class MainWindowViewModel : BaseViewModel
+    {   //свойство на которое байндиться текст контролов
+        public string SynchronizedText { get; set; } 
+    }
+}
